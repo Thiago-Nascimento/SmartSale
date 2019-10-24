b253e974ebeb712a2df9c4e21aa7029713e926ed
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2019-10-24 10:58:08 -0300

    Controller Regiao Concluido

--- backend/Models/BD_SmartSaleContext.cs
+++ backend/Models/BD_SmartSaleContext.cs
@@ -29,8 +29,7 @@ namespace backend.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=N-2S-REDES-17\\SQLEXPRESS;  Database=BD_SmartSale; User Id=sa; Password=132 ");
+                optionsBuilder.UseSqlServer("Server=N-1S-DEV-09\\SQLEXPRESS;  Database=BD_SmartSale; User Id=sa; Password=132 ");
             }
         }
 
