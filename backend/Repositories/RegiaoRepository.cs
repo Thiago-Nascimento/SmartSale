ea96050d02cad2a9c6e48a2f84ed24047adc2a8e
Vitor Martins <54907067+VitorMartinsAlves@users.noreply.github.com>
2019-10-30 15:51:08 -0300

    Ongs Repository

--- backend/Repositories/RegiaoRepository.cs
+++ backend/Repositories/RegiaoRepository.cs
@@ -2,34 +2,45 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using backend.Domains;
 using backend.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace backend.Repositories
-{
-    public class RegiaoRepository : IRegiao
-    {
-        public async Task<Regiao> Alterar(Regiao regiao)
-        {
-            throw new System.NotImplementedException();
+namespace backend.Repositories {
+    public class RegiaoRepository : IRegiao {
+        public async Task<Regiao> Alterar (Regiao regiao) {
+            using (BD_SmartSaleContext _contexto = new BD_SmartSaleContext ()) {
+                _contexto.Entry (regiao).State = EntityState.Modified;
+                await _contexto.SaveChangesAsync ();
+            }
+            return regiao;
         }
 
-        public async Task<Regiao> BuscarPorID(int id)
-        {
-            throw new System.NotImplementedException();
+        public async Task<Regiao> BuscarPorID (int id) {
+            using (BD_SmartSaleContext _contexto = new BD_SmartSaleContext ()) {
+                return await _contexto.Regiao.FindAsync (id);
+            }
         }
 
-        public async Task<Regiao> Excluir(Regiao regiao)
-        {
-            throw new System.NotImplementedException();
+        public async Task<Regiao> Excluir (Regiao regiao) {
+            using (BD_SmartSaleContext _contexto = new BD_SmartSaleContext ()) {
+                _contexto.Regiao.Remove (regiao);
+                await _contexto.SaveChangesAsync ();
+            }
+            return regiao;
         }
 
-        public async Task<List<Regiao>> Listar()
-        {
-            throw new System.NotImplementedException();
+        public async Task<List<Regiao>> Listar () {
+             using(BD_SmartSaleContext _contexto = new BD_SmartSaleContext()){
+                return await  _contexto.Regiao.ToListAsync();
+            }    
         }
 
-        public async Task<Regiao> Salvar(Regiao regiao)
-        {
-            throw new System.NotImplementedException();
+        public async Task<Regiao> Salvar (Regiao regiao) {
+            using(BD_SmartSaleContext _contexto = new BD_SmartSaleContext()){
+                await _contexto.AddAsync(regiao);
+                await _contexto.SaveChangesAsync();
+            }
+
+            return regiao;
         }
     }
 }
