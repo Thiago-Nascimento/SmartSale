a8cc81009f4eb63133a6d5baec558d8dacf04cf9
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2019-12-14 16:22:45 -0300

    Pagina Not Found feita

--- smart_sale/src/pages/not_found/not_found.js
+++ smart_sale/src/pages/not_found/not_found.js
@@ -8,7 +8,18 @@ class NotFound extends Component {
             <div className="Faq">
                 <Header {...this.props} />
                 <main>
-                    
+                    <div className="not_found">
+                        <div className="container">
+                            <div className="container1_perguntasfrequentes">
+                                <div className="texto_perguntasfrequentes">
+                                    <h1>HTTP - 404</h1>
+                                    <h1 id="not_found_h">Ops, não encontramos a página que você solicitou...</h1>
+                                    <p id="not_found_p">Verifique se digitou o endereço corretamente.</p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </main>
                 <Footer />
             </div>
