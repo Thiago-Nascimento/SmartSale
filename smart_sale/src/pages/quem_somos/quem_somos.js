62627467646ba7ccccf4a5c020c7370c887a9522
FIC <FIC@info.local>
2019-12-18 15:28:40 -0300

    css pag quem somos e ongs revisado

--- smart_sale/src/pages/quem_somos/quem_somos.js
+++ smart_sale/src/pages/quem_somos/quem_somos.js
@@ -9,33 +9,35 @@ class QuemSomos extends Component {
     render() {
         return (
             <div className="QuemSomos">
-            <Header {...this.props} />
+                <Header {...this.props} />
                 <section className="quemsomos_tudo">
                     <div className="quemsomos_pag">
                         <div className="imagem_quemsomos">
-                            <section className="quemsomos_texto_one">
-                                <h1>Objetivo</h1>
-                                <p>
-                                    O objetivo dessa aplicação trás ofertas de produtos até nossos clientes. Ofertas essas de
-                                    produtos
-                                    com data de validade próximas, portanto, com um preço mais acessível. Ótimo para os que
-                                    querem
-                                    economizar e evitar que produtos não tenham um destino sem consumo. Desenvolvido pela
-                                    empresa
-                                    -Faivi-, apresenta soluções envolvendo doações para ONGs e até mesmo um ranking de
-                                    vendedores e
-                                    clientes. Há vantagens de se utilizar a Smart Sale, entre elas a diminuição do desperdício
-                                    que
-                                    muitos produtos sofrem ao não serem consumidos ou descartados por perecerem. Com esta
-                                    solução,
-                                    estes
-                                    produtos são consumidos antes de perecerem, sejam por consumidores ou ONGs. Outra vantagem é
-                                    a
-                                    possibilidade de encontrar produtos em oferta e com um preço mais acessível. Além desses
-                                    benefícios,
-                                    um ranking apresenta a colocação de quem utiliza a aplicação e efetua doações.
+                            <div>
+                                <section className="quemsomos_texto_one">
+                                    <h1>Objetivo</h1>
+                                    <p>
+                                        O objetivo dessa aplicação trás ofertas de produtos até nossos clientes. Ofertas essas de
+                                        produtos
+                                        com data de validade próximas, portanto, com um preço mais acessível. Ótimo para os que
+                                        querem
+                                        economizar e evitar que produtos não tenham um destino sem consumo. Desenvolvido pela
+                                        empresa
+                                        -Faivi-, apresenta soluções envolvendo doações para ONGs e até mesmo um ranking de
+                                        vendedores e
+                                        clientes. Há vantagens de se utilizar a Smart Sale, entre elas a diminuição do desperdício
+                                        que
+                                        muitos produtos sofrem ao não serem consumidos ou descartados por perecerem. Com esta
+                                        solução,
+                                        estes
+                                        produtos são consumidos antes de perecerem, sejam por consumidores ou ONGs. Outra vantagem é
+                                        a
+                                        possibilidade de encontrar produtos em oferta e com um preço mais acessível. Além desses
+                                        benefícios,
+                                        um ranking apresenta a colocação de quem utiliza a aplicação e efetua doações.
                                 </p>
-                            </section>
+                                </section>
+                            </div>
                         </div>
                     </div>
                     <div className="quemsomos_partetwo">
@@ -64,7 +66,7 @@ class QuemSomos extends Component {
                         </section>
                     </div>
                 </section>
-                <Footer/>
+                <Footer />
             </div>
         );
     }
